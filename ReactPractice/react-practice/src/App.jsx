b557caf76e9ab7784f787d8a0b8a26e87d61bc2e
FourOfAKind <mrjamshodgson@gmail.com>
2025-03-03 14:56:24 +0000

    react practice

--- ReactPractice/react-practice/src/App.jsx
+++ ReactPractice/react-practice/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const Card = ({title, rating}) => {
+    const [interactions, setInteractions] = useState(0)
+    const [hasLiked, setHasLiked] = useState(false);
+    useEffect(() => {
+        console.log(`${title} has like set to ${hasLiked}`);
+    }, [hasLiked]);
     return (
-        <div className={"card"}>
+        <div className={"card"} onClick={() => setInteractions(interactions+1)}>
             <h2>{title} - {rating}/5</h2>
+            <button onClick={() => setHasLiked(!hasLiked)}>
+                {hasLiked ? "❤️" : "🤍"}
+            </button>
+            <h2>{interactions ? interactions : null}</h2>
         </div>
     )
 }
 
 const App = () => {
-  const [hasLiked, setHasLiked] = useState(false);
-
-
   return (
     <div className={"card-container"}>
         <Card title="Test" rating={5}/>
