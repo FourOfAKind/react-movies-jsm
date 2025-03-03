1f90739b49b17b370504bf9631314369eac7d93a
FourOfAKind <mrjamshodgson@gmail.com>
2025-03-03 15:33:10 +0000

    add index css snippet

--- ReactPractice/react-practice/src/App.jsx
+++ ReactPractice/react-practice/src/App.jsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-
-const Card = ({title, rating}) => {
-    const [interactions, setInteractions] = useState(0)
-    const [hasLiked, setHasLiked] = useState(false);
-    useEffect(() => {
-        console.log(`${title} has like set to ${hasLiked}`);
-    }, [hasLiked]);
+const App = () => {
     return (
-        <div className={"card"} onClick={() => setInteractions(interactions+1)}>
-            <h2>{title} - {rating}/5</h2>
-            <button onClick={() => setHasLiked(!hasLiked)}>
-                {hasLiked ? "❤️" : "🤍"}
-            </button>
-            <h2>{interactions ? interactions : null}</h2>
+        <div>
+            <h1 className={"text-3xl text-center font-bold underline"}>
+                Hello World
+            </h1>
         </div>
     )
 }
-
-const App = () => {
-  return (
-    <div className={"card-container"}>
-        <Card title="Test" rating={5}/>
-        <Card title="Test2" rating={4}/>
-    </div>
-  )
-}
-
 export default App
