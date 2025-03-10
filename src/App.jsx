2a41fd6ce13262340157e733b9d77897bda5c7dc
FourOfAKind <mrjamshodgson@gmail.com>
2025-03-10 23:15:04 +0000

    addTMDB api calls and movie component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
 import Search from "./components/Search.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Spinner from "./components/Spinner.jsx";
+import Movie from "./components/Movie.jsx";
+
+const TMDB_API_BASE_URL = "https://api.themoviedb.org/3/";
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const TMDB_API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        authorization: `Bearer ${TMDB_API_KEY}`
+    }
+}
 
 const App = () => {
     const[searchTerm, setSearchTerm] = useState("");
+    const[errorMessage, setErrorMessage] = useState("");
+    const[movies, setMovies] = useState([]);
+    const[isLoading, setIsLoading] = useState(true);
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage("");
+
+        try {
+            const endpoint = `${TMDB_API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const response = await fetch(endpoint, TMDB_API_OPTIONS);
+            if (!response.ok) {
+                console.log(`Failed to fetch movies. ${response.statusText}`);
+            } else {
+                const data = await response.json();
+                if(data.Response === "False") {
+                    setErrorMessage(data.Error || "Failed to fetch movies.");
+                    setMovies([]);
+                }
+                setMovies(data.results || []);
+            }
+        } catch (e) {
+            setErrorMessage(`Error fetching movies: ${e.message}`);
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, [])
     return (
         <main>
             <div className={"pattern"} />
@@ -11,9 +55,23 @@ const App = () => {
                 <header>
                     <img src={"./hero-img.png"} alt={"Hero"}/>
                     <h1>Find <span className={"text-gradient"}>Movies</span> that you&#39;ll love</h1>
-
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+                <section className={"all-movies"}>
+                    <h2 className={"mt-[20px]"}>All Movies</h2>
+                    {isLoading ? (
+                     <Spinner />
+                    ) : errorMessage ? (
+                        <p className={"text-white"}>{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movies.map((movie) => (
+                                <Movie key={movie.id} movie={movie}/>
+                            ))}
+                        </ul>
+                    )}
+                </section>
             </div>
         </main>
     )
---
+++ src/components/Movie.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line react/prop-types
+const Movie = ({movie:
+    { title, vote_average, poster_path, release_date, original_language}}) => {
+    return (
+        <div className={"movie-card"}>
+            <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title}/>
+            <h3 className={"text-xl mt-4"}>{title}</h3>
+            <div className={"content"}>
+                <img src={"/Rating.svg"} className={"w-5"} />
+                <h4 className={"text-gray-300"}>{vote_average ? vote_average.toFixed(1) : "N/A"}</h4>
+                <span>|</span>
+                <h4 className={"text-gray-300"}>{release_date ? release_date.split('-')[0] : "N/A"}</h4>
+                <span>|</span>
+                <h4 className={"text-gray-300"}>{original_language ? original_language.toUpperCase() : "N/A"}</h4>
+            </div>
+
+        </div>
+    )
+}
+export default Movie
